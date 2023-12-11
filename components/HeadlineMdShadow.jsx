2b8e519b9e233e9e2f7a3386aeddf6cff1a88b04
Miroljub Grujin <miroljub.grujin@bridgemaker.com>
2023-12-11 23:27:18 +0100

    font shadow fix

--- components/HeadlineMdShadow.jsx
+++ components/HeadlineMdShadow.jsx
@@ -1,6 +1,6 @@
 export default function HeadlineMdShadow(props) {
   return (
-    <props.attr className="text-7xl md:text-7xl font-extrabold tracking-wide blurred-text absolute top-16">
+    <props.attr className="text-6xl md:text-7xl font-montserrat font-headline-bold tracking-wide blurred-text absolute top-8 md:top-16">
       {props.title}
     </props.attr>
   );
